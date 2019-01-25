daf62bdc021485ac0c17082fd45c86b4834c53ae
Russell Miller <bet2buy@gmail.com>
2019-01-25 10:57:07 -0500

    represent your pet as an object

---
+++ script.js
@@ -0,0 +1,15 @@
+// Practice: Represent your Pet
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const myPet = {
+    name: "Miss Kitty",
+    species: "feline",
+    nickname:["miss kitty tooooo pretty", "Miss Meowser's", "king kitty"],
+    age: 8
+}
+
